11d9bdc89088a0c19b64afbb2d3394d3f54b8890
陈曦 <908382805@qq.com>
2020-11-20 13:57:23 +0800

    新增ngforage库，新增http服务

--- frontend/angular-ts/src/app/app-routing.module.ts
+++ frontend/angular-ts/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SelectivePreloadingStrategyService } from '../CX/services/selective-preloading-strategy.service';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        preloadingStrategy:  SelectivePreloadingStrategyService
+      }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
