79bc20c3e418ca0058ee6cb3b126d7014ed4e503
陈曦 <908382805@qq.com>
2020-12-29 14:02:25 +0800

    新增supermap使用 新增gis示例页面，视图默认观察地点为五龙新城

---
+++ frontend/angular-ts/src/app/pages/super-map-gis/super-map-gis.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuperMapGisRoutingModule } from './super-map-gis-routing.module';
+import { SuperMapGisComponent } from './super-map-gis.component';
+
+
+@NgModule({
+  declarations: [SuperMapGisComponent],
+  imports: [
+    CommonModule,
+    SuperMapGisRoutingModule
+  ]
+})
+export class SuperMapGisModule { }
